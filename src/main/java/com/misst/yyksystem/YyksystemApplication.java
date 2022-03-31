adb36ec85455a488a1fbbeb4ad5c48b34bdd9673
SilenceLoveLee <yangyukun0718@126.com>
2022-03-31 11:03:31 +0800

    第一次提交

---
+++ src/main/java/com/misst/yyksystem/YyksystemApplication.java
@@ -0,0 +1,13 @@
+package com.misst.yyksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyksystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyksystemApplication.class, args);
+    }
+
+}
